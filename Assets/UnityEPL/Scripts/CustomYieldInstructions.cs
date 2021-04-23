81fa2758365c01deeda2990fd4c962d76b116e42
James Bruska <jamesbthecoder@gmail.com>
2021-04-23 16:09:36 -0400

    Added CustomYieldInstruction called WaitUntilWithTimeout

---
+++ Assets/UnityEPL/Scripts/CustomYieldInstructions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class WaitUntilWithTimeout : CustomYieldInstruction
+{
+    private readonly Func<bool> condition;
+    private float timeout;
+
+    public WaitUntilWithTimeout(Func<bool> condition, float timeout)
+    {
+        this.condition = condition;
+        this.timeout = timeout;
+    }
+
+    public override bool keepWaiting
+    {
+        get
+        {
+            timeout -= Time.deltaTime;
+            //if (timeout <= 0f)
+                //throw new TimeoutException("WaitUntilWithTimeout timed out");
+            return !condition() && (timeout > 0f);
+        }
+    }
+
+    public bool timedOut() { return timeout <= 0f; }
+}

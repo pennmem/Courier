6a07ee9f80e5dfc73b5040e4516f7f30c0045036
zrentala <109314346+zrentala@users.noreply.github.com>
2026-06-16 16:57:06 -0400

    add back new version of deriving target mean

--- Assets/Scripts/DeliveryExperiment.cs
+++ Assets/Scripts/DeliveryExperiment.cs
@@ -291,8 +291,9 @@ public class DeliveryExperiment : CoroutineExperiment
         System.Random rng = new System.Random();
 
         // --- Targets for mean/SD ---
-        double targetMean = Math.Round(Config.targetMean[0] + rng.NextDouble() * (Config.targetMean[1] - Config.targetMean[0]));
         double targetSD = Config.targetVar[0] + rng.NextDouble() * (Config.targetVar[1] - Config.targetVar[0]);
+        double targetMean = Math.Round((1 + 2 * targetSD) + rng.NextDouble() * (49 - 4 * targetSD));
+
         double targetVar = targetSD * targetSD;
 
         int valMin = 1;
